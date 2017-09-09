9f36bc744da5001d925bcffc35c73aa8b0ff2f66
esgTom <esgTom@entara.com>
2017-09-09 08:47:23 -0500

    Checking in Bootstrap, FontAwesome, and other changes

---
+++ src/app/_models/column.ts
@@ -0,0 +1,21 @@
+export class Column {
+    ColumnAPIDataType: string;
+    ColumnAPIName: string;
+    ColumnClientDataType: string;
+    ColumnClientName: string;
+    ColumnDecimalPlaces: number;
+    ColumnDescription: string;
+    ColumnId: number;
+    ColumnLength: number;
+    ColumnName: string;
+    ColumnNullable: number;
+    ColumnOrdinalPosition: number;
+    ColumnPrimaryKey: number;
+    ColumnSQLDataType: string;
+    ColumnSQLName: string;
+    CreatedBy: string;
+    CreatedDate: string;
+    ModifiedBy: string;
+    ModifiedDate: string;
+    TableId: number;
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { GenerationComponent } from './generation/generation.component';
 import { NextGenRouteResolveGuard } from './services/next-gen-route.guard';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+    { path: 'home', component: HomeComponent },
     { path: 'codes', component: GenerationComponent, resolve: { codes: NextGenRouteResolveGuard}   },
     { path: 'metadata', loadChildren: 'app/meta-data/meta-data.module#MetaDataModule'},
-    { path: '', redirectTo: '/codes', pathMatch: 'full'},
-    { path: '**', redirectTo: '/codes'}
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '**', redirectTo: '/home'}
 ];
 
 @NgModule({
